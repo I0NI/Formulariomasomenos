afe7c7fc4a13ad790c56d33c6c1e60b8eb2c198e
I0NI <114189947+I0NI@users.noreply.github.com>
2023-10-04 20:53:17 -0600

    Add files via upload

---
+++ Formularionoob/out/production/Formularionoob/prosesar_registro.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $email = $_POST["email"];
+    $csvFile = "alumnos.csv";
+    $csvData = "$nombre,$apellido,$email\n";
+
+    if (file_put_contents($csvFile, $csvData, FILE_APPEND | LOCK_EX)) {
+        echo "Registro exitoso. Gracias por registrarte.";
+    } else {
+        echo "Error al registrar los datos.";
+    }
+}
+?>
+
